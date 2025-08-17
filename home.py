c5e59a62b0bc3ab339a5a405d6e1a0416d8defe5
Echo1usa <spencerg0516@gmail.com>
2025-08-16 23:27:40 -0600

    Experimenting with new data fields

--- home.py
+++ home.py
@@ -3,8 +3,8 @@ import sqlite3
 import pandas as pd
 
 # --- Page config ---
-st.set_page_config(page_title="WYCO 2025 Season Standings as of 7/19/2025", layout="centered")
-st.title("WYCO 2025 Season Standings as of 7/19/2025  THIS IS THE DEV BRANCH")
+st.set_page_config(page_title="WYCO 2025 Season Standings as of 8/16/2025", layout="centered")
+st.title("WYCO 2025 Season Standings as of 8/16/2025")
 
 # --- Connect to the database ---
 db_path = "allshooters_prs.db"
